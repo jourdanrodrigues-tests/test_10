fe6bdef542b942c235ca2ad527af910e50c499b8
Jourdan Rodrigues <thiagojourdan@gmail.com>
2018-11-15 22:12:08 +0100

    :sparkles: Add rating validation and fix recipe's

--- app/models.py
+++ app/models.py
@@ -5,23 +5,40 @@ from db.query import Model, Query
 class RecipeQuery(Query):
     def create(self, **data):
         # TODO: Implement this in Postgres
-        difficulty_choices = self.model.RECIPE_DIFFICULTIES_CHOICES
+        difficulty_choices = self.model.DIFFICULTIES_CHOICES
+
         if data.get('difficulty') not in difficulty_choices:
             first, last = difficulty_choices[0], difficulty_choices[-1]
             raise BadRequestError('Recipe difficulty should be between {} and {}'.format(first, last))
-        super().create(**data)
+
+        return super().create(**data)
 
 
 class Recipe(Model):
     query_class = RecipeQuery
     fields = ['id', 'name', 'difficulty', 'vegetarian', 'preparation_time']
 
-    RECIPE_DIFFICULTIES_CHOICES = [1, 2, 3]
+    DIFFICULTIES_CHOICES = [1, 2, 3]
 
 
 class User(Model):
     fields = ['id']
 
 
+class RatingQuery(Query):
+    def create(self, **data):
+        # TODO: Implement this in Postgres
+        rating_choices = self.model.RATING_CHOICES
+
+        if data.get('value') not in rating_choices:
+            first, last = rating_choices[0], rating_choices[-1]
+            raise BadRequestError('Rating value should be between {} and {}'.format(first, last))
+
+        return super().create(**data)
+
+
 class Rating(Model):
+    query_class = RatingQuery
     fields = ['id', 'recipe_id', 'value']
+
+    RATING_CHOICES = [1, 2, 3, 4, 5]
