1ad0980ca4f6705d79e7b366bc2e4ead69d76845
Jourdan Rodrigues <thiagojourdan@gmail.com>
2018-11-15 21:27:46 +0100

    :sparkles: Override recipe query and apply difficulty validation

--- app/models.py
+++ app/models.py
@@ -1,8 +1,21 @@
-from db.query import Model
+from core.exceptions import BadRequestError
+from db.query import Model, Query
+
+
+class RecipeQuery(Query):
+    def create(self, **data):
+        difficulty_choices = self.model.RECIPE_DIFFICULTIES_CHOICES
+        if data.get('difficulty') not in difficulty_choices:
+            first, last = difficulty_choices[0], difficulty_choices[-1]
+            raise BadRequestError('Recipe difficulty should be between {} and {}'.format(first, last))
+        super().create(**data)
 
 
 class Recipe(Model):
+    query_class = RecipeQuery
     fields = ['id', 'name', 'difficulty', 'vegetarian', 'preparation_time']
+
+    RECIPE_DIFFICULTIES_CHOICES = [1, 2, 3]
 
     def __init__(self, **kwargs):
         self.id = kwargs.get('id')
