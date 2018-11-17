0a7408c2a9b48de4e7e22e6f6df6ff35cf622e27
Jourdan Rodrigues <thiagojourdan@gmail.com>
2018-11-17 18:49:04 +0100

    :umbrella: Add tests for POST /recipes/

--- tests/integration/api/recipes/test_resource.py
+++ tests/integration/api/recipes/test_resource.py
@@ -1,6 +1,6 @@
 import requests
 
-from app.models import Recipe
+from app.models import Recipe, User
 
 
 class TestGet:
@@ -35,3 +35,36 @@ class TestGet:
             assert isinstance(data, list) and len(data) == 1
         finally:
             Recipe.query.filter(id__in=[id1, id2]).delete()
+
+
+class TestPost:
+    def test_when_no_authorization_header_is_sent_then_returns_unauthorized_response(self, server_host):
+        recipe_data = {}  # It won't get to the view anyway
+
+        response = requests.post(server_host + '/recipes/', json=recipe_data)
+        data = response.json()
+
+        assert data == {'detail': 'Authorization not sent or invalid.'}
+        assert response.status_code == 401
+
+    def test_when_authorized_and_data_is_valid_then_returns_data_with_created_id(self, server_host):
+        recipe_data = {
+            'name': 'A recipe name',
+            'difficulty': 3,
+            'vegetarian': False,
+            'preparation_time': 15,
+        }
+
+        user_id = User.query.create().id
+        headers = {'Authorization': str(user_id)}
+
+        response = requests.post(server_host + '/recipes/', json=recipe_data, headers=headers)
+        data = response.json()
+        expected_data = {'id': data['id'], **recipe_data}
+
+        try:
+            assert expected_data == data
+            assert response.status_code == 200
+        finally:
+            User.query.filter(id=user_id).delete()
+            Recipe.query.filter(id=data['id']).delete()
